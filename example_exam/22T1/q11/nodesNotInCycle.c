259306ca33f1d892d11043aa570095ae822fbfd7
paulinnnn12 <z5643770@ad.unsw.edu.au>
2026-08-15 19:51:49 +1000

    22T1 downloaded

---
+++ example_exam/22T1/q11/nodesNotInCycle.c
@@ -0,0 +1,14 @@
+
+#include <assert.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "Graph.h"
+
+// Worst case time complexity of this solution: O(...)
+void nodesNotInCycle(Graph g, int notInCycle[]) {
+    // TODO
+
+}
+
---
+++ example_exam/22T1/q9/zipList.c
@@ -0,0 +1,15 @@
+
+#include <assert.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "list.h"
+
+// Worst case time complexity of this solution: O(...)
+List zipList(List l1, int x, List l2, int y) {
+    // TODO
+
+    return ListNew();
+}
+
